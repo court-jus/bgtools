9644f89afd12a19be1d9fda5a140ee3450748c9e
Ghislain Lévêque <ghislain.leveque@clarisys.fr>
2016-06-29 11:42:34 +0200

    0.0.3

--- pnpdeliver/src/app/test/ctrl.js
+++ pnpdeliver/src/app/test/ctrl.js
@@ -12,18 +12,28 @@ angular.module('pnpdeliverApp')
     };
     $scope.model = {
       pageFormat: 'A4',
+      rows: 3,
+      cols: 3,
+      cardWidth: 2.5,
+      cardHeight: 3.5,
+      hMargin: 0,
+      vMargin: 0,
+      imageWidth: 2.5,
+      imageHeight: 3.5,
+      imageHShift: 0,
+      imageVShift: 0,
       // imagesUrls: 'https://thumbs.dreamstime.com/x/playing-card-back-side-60x90-mm-17679108.jpg\nhttp://www.jimknapp.com/Cards/Non-Bicycle_files/image002.jpg',
       imagesUrls: '',
       pages: []
     };
-    $scope.$watch('model.imagesUrls', function(n) {
+    $scope.$watch('model', function(n) {
       if (angular.isUndefined(n)) {
         return;
       }
       $scope.model.pages = [];
       var currentPage = [];
-      _.each(n.split('\n'), function(imageUrl) {
-        if (currentPage.length === 9) {
+      _.each(n.imagesUrls.split('\n'), function(imageUrl) {
+        if (currentPage.length === ($scope.model.rows * $scope.model.cols)) {
           $scope.model.pages.push({
             cards: currentPage
           });
@@ -34,12 +44,12 @@ angular.module('pnpdeliverApp')
         }
       });
       if (currentPage.length > 0) {
-        while (currentPage.length < 9) {
+        while (currentPage.length < ($scope.model.rows * $scope.model.cols)) {
           currentPage.push('assets/images/white.png');
         }
         $scope.model.pages.push({
           cards: currentPage
         });
       }
-    });
+    }, true);
   });
